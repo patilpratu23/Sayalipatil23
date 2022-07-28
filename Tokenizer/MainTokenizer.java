2124a53f156ca65684f9215b9bb8f4a7f3bdaefa
Sayalipatil23 <108451152+Sayalipatil23@users.noreply.github.com>
2022-07-28 00:18:16 -0700

    Add files via upload

--- Tokenizer/MainTokenizer.java
+++ Tokenizer/MainTokenizer.java
@@ -1,1 +1,35 @@
-
+import java.util.StringTokenizer;
+class StringM
+{
+	void display()
+	{
+		StringTokenizer st1=new StringTokenizer("My name is sayali"," ");
+		System.out.println("token of StringM class:");
+		while(st1.hasMoreTokens())
+		{
+			System.out.println(st1.nextToken());
+		}
+	}
+}
+class StringN
+{
+	void display1()
+	{
+		StringTokenizer st2=new StringTokenizer("Today is Monday"," ");
+		System.out.println("token of StringN class:");
+		while(st2.hasMoreTokens())
+		{
+			System.out.println(st2.nextToken());
+		}
+	}
+}
+class MainTokenizer
+{
+	public static void main(String args[])
+	{
+		StringM s1=new StringM();
+		StringN s2=new StringN();
+		s1.display();
+		s2.display1();
+	}
+}
